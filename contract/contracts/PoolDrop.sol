566e59d3500bfb3aa8d197be4f8be42682e17d80
Naiem <naiemk@gmail.com>
2020-06-22 22:37:47 -0400

    Flatten PoolDrop contract

--- contract/contracts/PoolDrop.sol
+++ contract/contracts/PoolDrop.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.16;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -7,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  * @dev Basic pool-drop contract to distribute an ERC20 amount to multiple accounts
  **/
 contract PoolDrop {
-    constructor() public {}
 
     function transferManyFrom(address _token, address _from, address[] memory _tos, uint256 _value)
     public returns (bool) {
@@ -15,7 +15,7 @@ contract PoolDrop {
         require(_from != address(0));
         require(_value > 0);
         IERC20 Token = IERC20(_token);
-        for (uint i=0; i<_tos.length; i++) {
+        for (uint i = 0; i < _tos.length; i++) {
             Token.transferFrom(_from, _tos[i], _value);
         }
         return true;
